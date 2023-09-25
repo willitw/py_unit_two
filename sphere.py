6f13c7c085704e077e2f62d7e530c95531c4de04
twig <willitw@ssfs.org>
2023-09-25 15:16:33 -0400

    finished triangle.py rectangle.py and sphere.py

--- sphere.py
+++ sphere.py
@@ -1,2 +1,8 @@
 # Follow the instructions on the Variables and Expressions sheet to calculate the volume of a sphere with
 # a given radius. As a hint, when doing something to the 3rd power, Python has a special operator. r^3 will NOT work.
+radius = 5
+r = 5
+pi = 3.14
+
+area = 4/3 * pi * r**3
+print("The area of the shphere is", area)
